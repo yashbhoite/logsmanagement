407fc542b6d4fbe4debedb53e7385a878999e255
Yash Bhoite <yash2505bhoite@gmail.com>
2026-02-23 15:15:41 +0530

    Added a route in logsgenerator to send logs to viewer to test CPU load and HPA on viewer service

--- logsgenerator/app.py
+++ logsgenerator/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import random
 import logging
 import requests
@@ -10,6 +10,8 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 PROCESSOR_URL = os.getenv("PROCESSOR_URL", "http://localhost:5001")
+VIEWER_URL = os.getenv("RECEIVER_URL","http://localhost:5002")
+DEFAULT_LOG_COUNT = int(os.getenv("DEFAULT_LOG_COUNT", 1))
 
 @app.route("/")
 def generate_log():
@@ -37,6 +39,44 @@ def generate_log():
         logging.exception("Failed to POST log to processor: %s", e)
 
     return {"status": "log generated"}
+
+
+@app.route("/direct-viewer")
+def send_logs_to_viewer():
+    count = int(request.args.get("count", DEFAULT_LOG_COUNT))  # default 1 log
+
+    logs_sent = 0
+
+    for _ in range(count):
+        log_type = random.choice(["INFO", "WARNING", "ERROR"])
+
+        if log_type == "INFO":
+            message = "User accessed homepage (direct viewer)"
+            logging.info(message)
+        elif log_type == "WARNING":
+            message = "High memory usage detected (direct viewer)"
+            logging.warning(message)
+        else:
+            message = "Application crashed unexpectedly (direct viewer)"
+            logging.error(message)
+
+        log = {
+            "type": log_type,
+            "message": message,
+            "timestamp": datetime.utcnow().isoformat() + "Z",
+        }
+
+        try:
+            requests.post(f"{VIEWER_URL}/receive", json=log, timeout=2)
+            logs_sent += 1
+        except Exception as e:
+            logging.exception("Failed to POST log to viewer: %s", e)
+
+    return {
+        "status": "completed",
+        "logs_requested": count,
+        "logs_sent": logs_sent
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
